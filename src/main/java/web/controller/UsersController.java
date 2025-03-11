a30f27c4c8bb02a5bc82cd1c14f7ccd1789c57b0
DaryaVot <darya.vdv8@gmail.com>
2025-03-11 14:49:47 +0300

    Up mapping

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -2,7 +2,6 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import web.dao.UserDAOImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,39 +13,44 @@ import web.service.UserService;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UsersController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @GetMapping("/")
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
     public String show(Model model) {
         List<User> users = userService.getAllUsers();
         model.addAttribute("allUsers", users);
         return "users";
     }
 
-    @PostMapping("/users/add")
+    @PostMapping("/add")
     public String add(
             @RequestParam String name,
             @RequestParam String lastName,
             @RequestParam int age) {
         User user = new User(name, lastName, age);
         userService.add(user);
-        return "redirect:/";
+        return "redirect:/users";
     }
 
-    @GetMapping("/users/update")
+    @GetMapping("/update")
     public String update(@RequestParam long id, Model model) {
         User user = userService.findById(id);
         if (user == null) {
-            return "redirect:/";
+            return "redirect:/users";
         }
         model.addAttribute("user", user);
         return "updateUser";
     }
 
-    @PostMapping("/users/update")
+    @PostMapping("/update/")
     public String saveUpdate(@RequestParam long id,
                              @RequestParam String name,
                              @RequestParam String lastName,
@@ -56,13 +60,13 @@ public class UsersController {
         user.setLastName(lastName);
         user.setAge(age);
         userService.update(user);
-        return "redirect:/";
+        return "redirect:/users";
     }
 
-    @PostMapping("/users/delete")
+    @PostMapping("/delete")
     public String delete(@RequestParam long id) {
         userService.delete(id);
-        return "redirect:/";
+        return "redirect:/users";
     }
 }
 
